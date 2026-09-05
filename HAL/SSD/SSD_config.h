1013c9a00663f38ad184f191fef2bc80abf8953f
Mohamed Omar <mo455790@gmail.com>
2026-09-05 05:33:29 +0300

    add SSD driver

---
+++ HAL/SSD/SSD_config.h
@@ -0,0 +1,17 @@
+/*
+*
+*   <<<<<<<<<<<<<<<<<<<<<    SSD_config.h    >>>>>>>>>>>>>>>>>>>>>>>>>
+*
+*   Author  : Mohamed Omar
+*   layer   : HAL
+*   SWC     : SSD
+ */
+
+#ifndef _SSD_CONFIG_H_
+#define _SSD_CONFIG_H_
+
+
+
+
+
+#endif
---
+++ HAL/SSD/SSD_private.h
@@ -0,0 +1,19 @@
+/*
+*
+*   <<<<<<<<<<<<<<<<<<<<<    SSD_private.h    >>>>>>>>>>>>>>>>>>>>>>>>>
+*
+*   Author  : Mohamed Omar
+*   layer   : HAL
+*   SWC     : SSD
+ */
+
+#ifndef _SSD_PRIVATE_H_
+#define _SSD_PRIVATE_H_
+
+
+// common cathode
+#define SSD_NUMBER_ARR	{0b00111111 , 0b00000110 , 0b01011011 , 0b01001111 , 0b01100110 , 0b01101101 , 0b01111101 , 0b00000111 , 0b01111111 , 0b01101111}
+
+
+
+#endif
